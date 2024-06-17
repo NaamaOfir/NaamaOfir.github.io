2576753e64349817bdc1cfaec8cd4ae2dfae129a
Yoav Rabby <arikhativa@gmail.com>
2024-06-17 12:59:52 +0300

    better gallery

--- src/app/strings/videos.ts
+++ src/app/strings/videos.ts
@@ -2,4 +2,4 @@ export const filter1 = "Motion Design"
 export const filter2 = "3D Character Animation"
 export const filter3 = "Video Projects"
 
-export const defaultVideoSrc = "https://player.vimeo.com/video/830933663?h=35955ad039"
+export const defaultVideoSrc = "https://player.vimeo.com/video/950849766?h=0fad089f95"
--- src/app/video-gallery/video-gallery.component.ts
+++ src/app/video-gallery/video-gallery.component.ts
@@ -19,6 +19,7 @@ type VideoPreview = {
   name: string;
   path: string;
   url: string;
+  isSelected: boolean;
 }
 
 @Component({
@@ -31,6 +32,11 @@ export class VideoGalleryComponent {
 
   filters: Filter[] = [
     {
+      type: Type.VP,
+      name: filter3,
+      isSelected: true,
+    },
+    {
       type: Type.MD,
       name: filter1,
       isSelected: false,
@@ -38,11 +44,6 @@ export class VideoGalleryComponent {
     {
       type: Type.CA,
       name: filter2,
-      isSelected: true,
-    },
-    {
-      type: Type.VP,
-      name: filter3,
       isSelected: false,
     },
   ]
@@ -54,88 +55,103 @@ export class VideoGalleryComponent {
       type: Type.MD,
       path: 'assets/thumbnail/capture1.png',
       name: 'Pharmaceutical Company Animation',
-      url: 'https://player.vimeo.com/video/950842039?h=5f38d031f2'
+      url: 'https://player.vimeo.com/video/950842039?h=5f38d031f2',
+      isSelected: false,
     },
     {
       type: Type.MD,
       path: 'assets/thumbnail/capture2.png',
       name: 'Frog Animation',
-      url: 'https://player.vimeo.com/video/950843409?h=122c556dfd'
+      url: 'https://player.vimeo.com/video/950843409?h=122c556dfd',
+      isSelected: false,
     },
     {
       type: Type.MD,
       path: 'assets/thumbnail/capture3.png',
       name: 'Working From Home Animation',
-      url: 'https://player.vimeo.com/video/950843968?h=ff06ee5cac'
+      url: 'https://player.vimeo.com/video/950843968?h=ff06ee5cac',
+      isSelected: false,
     },
     {
       type: Type.MD,
       path: 'assets/thumbnail/capture4.png',
       name: 'Blue',
-      url: 'https://player.vimeo.com/video/950846203?h=661abe13f9'
+      url: 'https://player.vimeo.com/video/950846203?h=661abe13f9',
+      isSelected: false,
     },
     {
       type: Type.MD,
       path: 'assets/thumbnail/capture5.png',
       name: 'Mind-Body Connection Explainer',
-      url: ''
+      url: 'https://player.vimeo.com/video/950847796?h=a27afd3b38',
+      isSelected: false,
     },
     {
       type: Type.MD,
       path: 'assets/thumbnail/capture6.png',
       name: 'Moon Cycle Animation',
-      url: 'https://player.vimeo.com/video/950846845?h=e16980c954'
+      url: 'https://player.vimeo.com/video/950846845?h=e16980c954',
+      isSelected: false,
     },
     {
       type: Type.MD,
       path: 'assets/thumbnail/capture7.png',
       name: 'Windmill Animation',
-      url: ''
+      url: 'https://player.vimeo.com/video/950848729?h=25e3d48954',
+      isSelected: false,
     },
     {
       type: Type.MD,
       path: 'assets/thumbnail/capture8.png',
       name: 'Name Animation',
-      url: ''
+      url: 'https://player.vimeo.com/video/952260773?h=8fa4cdb0d4',
+      isSelected: false,
     },
     {
       type: Type.MD,
       path: 'assets/thumbnail/capture9.png',
       name: 'Dialogue Opener',
-      url: 'https://player.vimeo.com/video/830933663?h=35955ad039'
+      url: 'https://player.vimeo.com/video/830933663?h=35955ad039',
+      isSelected: false,
     },
     {
       type: Type.CA,
       path: 'assets/thumbnail/capture10.png',
       name: '3D Character Animation Reel 2024',
-      url: 'https://vimeo.com/830933663'
+      url: 'https://player.vimeo.com/video/886390809?h=7a97e72b5a',
+      isSelected: false,
     },
     {
       type: Type.VP,
       path: 'assets/thumbnail/capture11.png',
       name: 'Hailo-Holiday Short',
-      url: 'https://vimeo.com/950849766'
+      url: 'https://player.vimeo.com/video/950849766?h=0fad089f95',
+      isSelected: true,
     },
     {
       type: Type.VP,
       path: 'assets/thumbnail/capture12.png',
-      name: 'Hailo-Demo ',
-      url: 'https://vimeo.com/905515939'
+      name: 'Hailo-Demo',
+      url: 'https://player.vimeo.com/video/905515939?h=f8381f3038',
+      isSelected: false,
     },
     {
       type: Type.VP,
       path: 'assets/thumbnail/capture13.png',
-      name: 'Hailo-Renesas demo',
-      url: 'https://vimeo.com/905509689'
+      name: 'Hailo-Renesas Demo',
+      url: 'https://player.vimeo.com/video/905509689?h=96b7eb38e3',
+      isSelected: false,
     },
   ]
 
 
   constructor() {
-    this.videos = this.allVideos.filter((e) => e.type === Type.CA)
+    this.videos = this.allVideos.filter((e) => e.type === Type.VP)
   }
 
   handleVideoClick = (v: VideoPreview) => {
+    this.allVideos.forEach((e) => e.isSelected = false)
+    v.isSelected = true
     this.onSelectVideo(v.url)
   }
 
