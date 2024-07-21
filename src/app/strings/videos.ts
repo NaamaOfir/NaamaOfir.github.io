dae4d830da40844fccaf626372db7372105385ce
Yoav Rabby <arikhativa@gmail.com>
2024-07-21 14:13:32 +0300

    looks good

--- src/app/strings/videos.ts
+++ src/app/strings/videos.ts
@@ -2,4 +2,4 @@ export const filter1 = "Motion Design"
 export const filter2 = "3D Character Animation"
 export const filter3 = "Video Projects"
 
-export const defaultVideoSrc = "https://player.vimeo.com/video/950849766?h=0fad089f95"
+export const defaultVideoSrc = "https://player.vimeo.com/video/950842039?h=5f38d031f2"
--- src/app/video-data/video-data.service.ts
+++ src/app/video-data/video-data.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { JsonStruct } from '../video-gallery/video-gallery.component';
 
 @Injectable({
   providedIn: 'root'
@@ -10,7 +11,7 @@ export class VideoDataService {
   constructor(private http: HttpClient) {
   }
 
-  public getJSON(): Observable<unknown> {
-    return this.http.get("assets/data.json");
+  public getJSON(): Observable<JsonStruct> {
+    return this.http.get<JsonStruct>("assets/data.json");
   }
 }
